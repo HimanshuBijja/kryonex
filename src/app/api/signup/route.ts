73d6f6fa6e36e48c1d686f7ec67b16121c7aab99
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-08-12 22:54:35 +0530

    init

---
+++ src/app/api/signup/route.ts
@@ -0,0 +1,106 @@
+import dbConnect from '@/lib/dbConnect';
+import UserModel from '@/model/User';
+import { NextRequest, NextResponse } from 'next/server';
+import  bcrypt from 'bcryptjs';
+import VerificationEmail from '../../../../emails/verificationEmail';
+import { sendVerificationEmail } from '@/helpers/sendVerificationEmail';
+import { success } from 'zod';
+ 'bcryptjs';
+
+export async function POST(request: NextRequest) {
+    await dbConnect();
+
+    try {
+        const { email, username, password } = await request.json();
+        const existingUserVerifiedByUsername = await UserModel.findOne({
+            username,
+            isVerified: true,
+        });
+        if (existingUserVerifiedByUsername) {
+            return NextResponse.json(
+                {
+                    success: false,
+                    message: 'Username already taken',
+                },
+                {
+                    status: 400,
+                },
+            );
+        }
+
+        const existingUserByEmail = await UserModel.findOne({
+            email
+        })
+        //genereate verifycode
+        const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+        if(existingUserByEmail){
+            
+            //if verified return
+            if(existingUserByEmail.isVerified){
+                return NextResponse.json({
+                    success: false,
+                    message: 'Email already verified'
+                })
+            }else{
+                const hashedPassword = await bcrypt.hash(password, 10);
+                existingUserByEmail.password = hashedPassword; // check why we are sending password here again when we send while creating user
+                existingUserByEmail.verifyCode = verifyCode;
+                existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000);
+                await existingUserByEmail.save();
+            }
+            //else send verification code in db
+        }else{
+            //encrypt pass  by 10 rounds
+            const hashedPassword = await bcrypt.hash(password, 10)
+            //expiry date
+            const expiryDate = new Date();
+            expiryDate.setHours(expiryDate.getHours() + 1);
+            //store user in db
+            
+            const newUser = new UserModel({
+                username,
+                email,
+                password: hashedPassword,
+                verifyCode: '123456',
+                verifyCodeExpiry: expiryDate,
+                isVerified: false,
+                isAcceptingMessage: true,
+                messages: [],
+            });
+            
+            await newUser.save();
+        }
+        
+        //send verification code 
+        const emailResponse = await sendVerificationEmail(email, username, verifyCode);
+        
+        if(!emailResponse.success){
+            return NextResponse.json({
+                success: false,
+                message: emailResponse.message
+            },{
+                status: 500
+            })
+         }
+
+         return NextResponse.json({
+            success: true,
+            message: 'User Registered Successfully, Please Verify Your Email'
+         } ,{
+            status: 201
+         })
+
+
+    } catch (error) {
+        console.error('Error Registering user', error);
+        return NextResponse.json(
+            {
+                success: false,
+                message: 'Error Registering user',
+            },
+            {
+                status: 500,
+            },
+        );
+    }
+}
