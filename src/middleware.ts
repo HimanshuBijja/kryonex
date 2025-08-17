c530a6ae5eca14831b58005de39f4b518b4cb6e7
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-08-17 21:54:12 +0530

    root middleware init

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,34 @@
+import { getToken } from 'next-auth/jwt';
 import { NextResponse, NextRequest } from 'next/server';
 
+//default from next-auth/middleware
+//jwt from next-auth/jwt
+
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
+    const token = await getToken({ req: request });
+    //get current url
+    const url = request.nextUrl;
+    //redirect strategy
+    if (
+        token &&
+        (url.pathname.startsWith('/sign-in') ||
+            url.pathname.startsWith('/sign-up') ||
+            url.pathname.startsWith('/verify') ||
+            url.pathname.startsWith('/'))
+    ) {
+        return NextResponse.redirect(new URL('/home', request.url));
+    }
+
     return NextResponse.redirect(new URL('/home', request.url));
 }
 
 export const config = {
-    matcher: '/about/:path*',
+    matcher: [
+        '/sign-in',
+        '/sign-up',
+        '/',
+        '/dashboard/:path*',
+        '/verify/:path*',
+    ],
 };
