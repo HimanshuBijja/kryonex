9e2e314266eb49c7f4a1edb799c40d0fc34142ca
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-08-28 23:25:49 +0530

    custom scroll bar and layout changes

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -8,11 +8,10 @@ export default function RootLayout({
   return (
     <>
       <Navbar />
-      <div className="mx-auto max-w-[1440px] border-2 my-6 min-h-screen">
-
-      {children}
-      </div>
-      {/* <footer>hello</footer> */}
+      <div className="mx-auto max-w-[1440px] border-2 my-6">{children}</div>
+      <footer className=" absolute -bottom-10 w-full">
+        <div className=" bg-black/80 text-white">hello</div>
+      </footer>
     </>
   );
 }
