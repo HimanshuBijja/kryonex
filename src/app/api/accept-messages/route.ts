c2ab892b28d56b122513717915c96e5ab0a75598
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-08-30 19:05:56 +0530

    added message field in response

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -111,6 +111,7 @@ export async function GET(request: Request) {
         return Response.json(
             {
                 success: true,
+                message: "User found",
                 isAcceptingMessages: foundUser.isAcceptingMessages,
             },
             {
