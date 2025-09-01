ee164cb9bb36deea8cd50f4a805a2dd7bb814e29
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-09-01 19:48:10 +0530

    db connection logs

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -18,10 +18,10 @@ async function dbConnect(): Promise<void> {
         const db = await mongoose.connect(process.env.MONGODB_URI || '', {})
 
         connection.isConnected = db.connections[0].readyState
-        // console.log("Database connected successfully");
+        console.log("Database connected successfully");
     }
     catch(error){
-        // console.error("Database connection failed:", error);
+        console.error("Database connection failed:", error);
         process.exit(1);
     }
 }
